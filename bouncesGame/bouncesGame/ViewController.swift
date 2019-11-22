932986b7254590f84cb70b2cd3883e6495c340b2
pjhubs <877302410@qq.com>
2019-11-22 22:21:52 +0800

    UIKit 物理模拟能力 & 2D 游戏常规概念

--- bouncesGame/bouncesGame/ViewController.swift
+++ bouncesGame/bouncesGame/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var animator: UIDynamicAnimator?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        animator = UIDynamicAnimator(referenceView: view)
+        let redView = UIView(frame: CGRect(x: 100, y: 300, width: 100, height: 100))
+        redView.backgroundColor = .red
+        view.addSubview(redView)
+        let gravity = UIGravityBehavior(items: [redView])
+        animator?.addBehavior(gravity)
+        
+        let collision = UICollisionBehavior(items: [redView])
+        collision.translatesReferenceBoundsIntoBoundary = true
+        self.animator?.addBehavior(collision)
+        
+//        collision.setTranslatesReferenceBoundsIntoBoundary(with: UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50))
+        
+        let anchor = CGPoint(x: self.view.bounds.width / 2, y: 400)
+        let attachment = UIAttachmentBehavior(item: redView, attachedToAnchor: anchor)
+        self.animator?.addBehavior(attachment)
     }
 
 
